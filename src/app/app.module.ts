678801a323be27f41c83ea4ae6a11e077035d502
ZacJocelyn <zacjocelyn@gmail.com>
2017-07-22 14:03:15 -0500

    added text editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
@@ -10,7 +11,13 @@ import { BlogComponent } from './blog/blog.component';
     BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'blog',
+        component: BlogComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
