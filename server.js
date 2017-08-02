fa337697a5e0c32d2a6f3718ae58e78fa940c63b
Lamont <matthew.lamont@travelport.com>
2017-08-01 20:28:30 -0600

    setup server for app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
